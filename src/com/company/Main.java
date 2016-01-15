c89b9de9ff52e08d4450cf35beae7ca8a0f15a28
jonathan <jxf131930@utdallas.edu>
2016-01-14 20:29:06 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // the last number is a place holder
+        int a[] = {1,2,3,4,5,6,7,8,9,0};
+
+
+        int temp1 = a[0];
+
+        for(int i = 0; i < a.length; i++){
+
+            if( i < 9) {
+                int temp2 = a[i + 1];
+                a[i + 1] = temp1;
+                temp1 = temp2;
+            }
+        }
+
+        //clear a[0] since it's value was shifted
+        a[0] = 0;
+
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + ",");
+        }
+
+
+    }
+}
